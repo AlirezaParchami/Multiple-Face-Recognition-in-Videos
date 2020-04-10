a10ce62da0d8ed0dfe1145b7d8f560b97a44b4d2
alireza parchami <alirezaprm76@gmail.com>
2020-04-10 19:04:40 +0430

    Add new person to trained data Feature

---
+++ Add new person to trained data.py
@@ -0,0 +1,35 @@
+import pickle
+import os
+import face_recognition
+
+dir = os.getcwd()
+
+with open('encoded_people.pickle', 'rb') as filename:
+    people = pickle.load(filename)
+
+print("Put the photos of new person in a folder. Set the name of the folder as the person's name. Drop the folder to People Folder.")
+newPersonName = input("Enter new person's name: ")
+
+people_dir = dir + '/people/'
+if not os.path.isdir(people_dir):
+    print("There is no 'People' folder")
+    exit(0)
+if newPersonName not in os.listdir(people_dir):
+    print("There is no folder with the given name in the 'People' folder")
+    exit(0)
+
+person_dir = people_dir + newPersonName + '/'
+person_faces = list()
+for photo in os.listdir(person_dir):
+    image = face_recognition.load_image_file(person_dir + photo)
+    face_encodings = face_recognition.face_encodings(image)
+    if len(face_encodings) > 0:
+        person_faces.append(face_encodings[0])
+if len(person_faces) == 0:
+    print("No faces were found.")
+else:
+    print(len(person_faces))
+    people[newPersonName] = person_faces
+
+with open('encoded_people.pickle', 'wb') as filename:
+    pickle.dump(people, filename)
--- Face Encoding.py
+++ Face Encoding.py
@@ -17,7 +17,6 @@ for person in os.listdir(people_dir):
     for photo in os.listdir(person_dir):
         print(photo)
         image = face_recognition.load_image_file(person_dir + photo)
-        face_locations = face_recognition.face_locations(image)  # for test
         face_encodings = face_recognition.face_encodings(image)
         if len(face_encodings) > 0:
             person_faces.append(face_encodings[0])
@@ -29,13 +28,5 @@ for person in os.listdir(people_dir):
         people[name] = person_faces
 
 # Pickling (serializing) a dictionary into a file
-with open('output.pickle', 'wb') as filename:
+with open('encoded_people.pickle', 'wb') as filename:
     pickle.dump(people, filename)
-'''
-face_img = PIL.Image.fromarray(image)
-draw = PIL.ImageDraw.Draw(face_img)
-for face in face_locations:
-    draw.rectangle(face,outline="red",width=2)
-    draw.text((face[0],face[1]), "Name",font=font)
-face_img.show()
-'''
